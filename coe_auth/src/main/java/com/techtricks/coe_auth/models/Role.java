1217cfc514f6861b9bb4d28a52a4911b0c873499
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-27 23:56:57 +0530

    add: added userDto for to authenticate the user purpose

---
+++ coe_auth/src/main/java/com/techtricks/coe_auth/models/Role.java
@@ -0,0 +1,23 @@
+package com.techtricks.coe_auth.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+public enum Role {
+
+    ADMIN(Set.of(Permission.READ, Permission.WRITE, Permission.DELETE)),
+    STUDENT(Set.of(Permission.READ)),
+    STAFF(Set.of(Permission.READ));
+
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+}
