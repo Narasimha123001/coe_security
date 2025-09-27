2e8d17cd699b2f1e287fbfc30e5fa821bff94d8e
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-28 00:13:42 +0530

    add: added the userRepository for user data storeing into database

---
+++ coe_auth/src/main/java/com/techtricks/coe_auth/repositorys/UserDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.techtricks.coe_auth.repositorys;
+
+import com.techtricks.coe_auth.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
