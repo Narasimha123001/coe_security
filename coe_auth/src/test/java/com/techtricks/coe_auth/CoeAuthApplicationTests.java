77a5248dea121ac61fc8424151a6b691de689021
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-27 13:09:08 +0530

    first commit -the boiler plate

---
+++ coe_auth/src/test/java/com/techtricks/coe_auth/CoeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtricks.coe_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
