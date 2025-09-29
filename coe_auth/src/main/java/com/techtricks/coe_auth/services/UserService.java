3f637f8ccdecec1172ce1b77504b547e9deca636
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-29 23:57:45 +0530

    update: remove the all method impletion and written only method declartions

--- coe_auth/src/main/java/com/techtricks/coe_auth/services/UserService.java
+++ coe_auth/src/main/java/com/techtricks/coe_auth/services/UserService.java
@@ -1,32 +1,22 @@
 package com.techtricks.coe_auth.services;
-
-import com.techtricks.coe_auth.models.Role;
+import com.techtricks.coe_auth.dtos.UserDto;
+import com.techtricks.coe_auth.dtos.UserResponseDto;
+import com.techtricks.coe_auth.exceptions.UserNotFoundException;
+import com.techtricks.coe_auth.exceptions.UserAlreadyPresentException;
 import com.techtricks.coe_auth.models.User;
-import com.techtricks.coe_auth.repositorys.UserDetailsRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
-public class UserService {
+public interface  UserService {
 
-    private final UserDetailsRepository userDetailsRepository;
-    private final PasswordEncoder passwordEncoder;
+   // public void save(User user);
 
-    public UserService(UserDetailsRepository userDetailsRepository, PasswordEncoder passwordEncoder) {
-        this.userDetailsRepository = userDetailsRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    List<UserResponseDto> getAllUser();
 
-    public void save(User user) {
-        if(user.getUsername() == null || user.getUsername().isEmpty()){
-            throw new IllegalArgumentException("Username cannot be empty");
-        }
-        User newUser = new User();
-        newUser.setUsername(user.getUsername());
-        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
-        newUser.setRole(user.getRole() != null ? user.getRole() : Role.STUDENT);
-        userDetailsRepository.save(newUser);
-    }
+    User getUserById(Long registerNumber)throws UserNotFoundException;
 
+    UserResponseDto saveUser(UserDto dto) throws UserAlreadyPresentException;
 
+    public User updateUser(Long registerNumber , User user) throws UserNotFoundException;
 }
