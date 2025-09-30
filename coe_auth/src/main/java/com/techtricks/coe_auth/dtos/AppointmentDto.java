e110c4569f9d99d9d5ca29aa6674f40fc5c60f94
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-30 22:49:34 +0530

    add: added the dto for appointment request data

---
+++ coe_auth/src/main/java/com/techtricks/coe_auth/dtos/AppointmentDto.java
@@ -0,0 +1,12 @@
+package com.techtricks.coe_auth.dtos;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class AppointmentDto {
+    private String purpose;
+}
