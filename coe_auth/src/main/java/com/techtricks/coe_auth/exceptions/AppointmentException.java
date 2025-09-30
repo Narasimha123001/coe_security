fd58c7434b4c158e37fa65df593d1c909f7430e0
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-30 22:59:10 +0530

    add: added the appoiment exception for the exception halding

---
+++ coe_auth/src/main/java/com/techtricks/coe_auth/exceptions/AppointmentException.java
@@ -0,0 +1,7 @@
+package com.techtricks.coe_auth.exceptions;
+
+public class AppointmentException extends Exception {
+    public AppointmentException(String message) {
+        super(message);
+    }
+}
