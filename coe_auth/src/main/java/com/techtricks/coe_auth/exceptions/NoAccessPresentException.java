71b0d063df59653394790712cf0aef520478528b
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-10-03 02:33:13 +0530

    add:added the no access exception is handle for the user

---
+++ coe_auth/src/main/java/com/techtricks/coe_auth/exceptions/NoAccessPresentException.java
@@ -0,0 +1,7 @@
+package com.techtricks.coe_auth.exceptions;
+
+public class NoAccessPresentException extends Throwable {
+    public NoAccessPresentException(String message) {
+        super(message);
+    }
+}
